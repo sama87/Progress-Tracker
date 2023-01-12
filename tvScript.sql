5bc16347e03f9cfc6285057dbcfd0a47ef1ea934
Matteo Antunez <antunezmatteo@gmail.com>
2023-01-12 13:38:02 -0600

    Updated SQL script

--- tvScript.sql
+++ tvScript.sql
@@ -1,22 +1,59 @@
-## DROP DATABASE TV_Show_Tracker;
+
+DROP DATABASE TV_Show_Tracker;
 
 CREATE DATABASE TV_Show_Tracker;
 
 USE TV_Show_Tracker;
 
+CREATE TABLE TV_SHOW (
+	tv_id INT NOT NULL,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (tv_id)
+);
+
 CREATE TABLE HAS_SHOW(
 	username VARCHAR(50) NOT NULL,
-    id INT NOT NULL,
+    tv_id INT NOT NULL,
     percentage_completed INT,
-    PRIMARY KEY (username, id)
+    PRIMARY KEY (username, tv_id),
+    FOREIGN KEY (tv_id) REFERENCES TV_SHOW(tv_id)
 );
 
 SHOW TABLES;
 
-CREATE TABLE TV_SHOW (
-	id INT NOT NULL,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (TV_SHOW.id) REFERENCES HAS_SHOW(HAS_SHOW.id)
+
+CREATE TABLE EPISODE (
+	episode_id INT NOT NULL,
+    name VARCHAR(50),
+    description VARCHAR(255), 
+    PRIMARY KEY (episode_id)
 );
+
+
+CREATE TABLE SEASON (
+	tv_id INT NOT NULL,
+    episode_id INT NOT NULL,
+    season_start DATE,
+    season_end DATE,
+    PRIMARY KEY (tv_id, episode_id),
+    FOREIGN KEY (tv_id) REFERENCES TV_SHOW(tv_id),
+    FOREIGN KEY (episode_id) REFERENCES EPISODE(episode_id)
+);
+
+
+CREATE TABLE TRACKER (
+	username VARCHAR(50) NOT NULL,
+    episode_id int NOT NULL,
+    completed boolean,
+    in_progress boolean,
+    not_started boolean,
+    PRIMARY KEY (username, episode_id)
+);
+
+CREATE TABLE USER (
+	username VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES HAS_SHOW(username),
+    FOREIGN KEY (username) REFERENCES TRACKER(username)
+);
+
