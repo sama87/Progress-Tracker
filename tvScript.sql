3ed94878127a9a22599201a4f23897036ea9d272
Matteo Antunez <antunezmatteo@gmail.com>
2023-01-13 10:51:23 -0600

    Added data into database; Fixed Logical issue

--- tvScript.sql
+++ tvScript.sql
@@ -20,16 +20,24 @@ CREATE TABLE HAS_SHOW(
     FOREIGN KEY (tv_id) REFERENCES TV_SHOW(tv_id)
 );
 
+DROP TABLE EPISODE;
 
 CREATE TABLE EPISODE (
 	tv_id INT NOT NULL,
 	episode_id INT NOT NULL,
     name VARCHAR(50),
     description TEXT, 
-    PRIMARY KEY (tv_id, episode_id),
-    FOREIGN KEY (tv_id) REFERENCES TV_SHOW(tv_id)
+    PRIMARY KEY (episode_id)
 );
 
+CREATE TABLE SEASON (
+	tv_id INT NOT NULL,
+    episode_id INT NOT NULL,
+    FOREIGN KEY (tv_id) REFERENCES TV_SHOW(tv_id),
+    FOREIGN KEY (episode_id) REFERENCES EPISODE(episode_id)
+);
+
+select * from episode;
 
 CREATE TABLE TRACKER (
 	username VARCHAR(50) NOT NULL,
@@ -46,5 +54,9 @@ CREATE TABLE USER (
     FOREIGN KEY (username) REFERENCES HAS_SHOW(username),
     FOREIGN KEY (username) REFERENCES TRACKER(username)
 );
+
+ow import failed with error: ('Cannot add or update a child row: a foreign key constraint fails (`tv_show_tracker`.`episode`, CONSTRAINT `episode_ibfk_1` FOREIGN KEY (`tv_id`) REFERENCES `tv_show` (`tv_id`))', 1452)
+- Row import failed with error: ('Cannot add or update a child row: a foreign key constraint fails (`tv_show_tracker`.`episode`, CONSTRAINT `episode_ibfk_1` FOREIGN KEY (`tv_id`) REFERENCES `tv_show` (`tv_id`))', 1452)
+
 
 select * from tv_show;
